c6f57620e2bfb8203c0c9737a7a484d2f4097566
crelle <383362161@qq.com>
2020-05-11 19:05:53 +0800

    模板修改

--- restful/src/main/java/com/crelle/restful/controller/RestfulController.java
+++ restful/src/main/java/com/crelle/restful/controller/RestfulController.java
@@ -3,6 +3,7 @@ package com.crelle.restful.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.xml.ws.RequestWrapper;
@@ -19,8 +20,9 @@ import javax.xml.ws.RequestWrapper;
 public class RestfulController {
 
     @RequestMapping("/hello")
-    public String hello(HttpServletRequest request, @RequestParam(value = "name", defaultValue = "springboot-thymeleaf") String name) {
+    public ModelAndView hello(HttpServletRequest request, @RequestParam(value = "name", defaultValue = "springboot-thymeleaf") String name) {
+        ModelAndView modelAndView = new ModelAndView("home");
         request.setAttribute("name", name);
-        return "home";
+            return modelAndView;
     }
 }
