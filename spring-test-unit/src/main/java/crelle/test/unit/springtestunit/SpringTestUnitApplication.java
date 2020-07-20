edd834528d2081f36308c1a383fe94a90dc8af8c
e <383362161@qq.com>
2020-07-20 23:03:35 +0800

    junit

---
+++ spring-test-unit/src/main/java/crelle/test/unit/springtestunit/SpringTestUnitApplication.java
@@ -0,0 +1,13 @@
+package crelle.test.unit.springtestunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestUnitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTestUnitApplication.class, args);
+    }
+
+}
