36625907719593db30ed55b387c2c39cad6ece67
e <383362161@qq.com>
2020-10-09 16:06:54 +0800

    spring session and security

---
+++ spring-projects-reference-test/spring-security-refrence-test/src/main/java/crelle/test/spring/security/reference/demo/MvcConfig.java
@@ -0,0 +1,11 @@
+package crelle.test.spring.security.reference.demo;
+
+/**
+ * @author:crelle
+ * @className:MvcConfig
+ * @version:1.0.0
+ * @date:2020/10/9
+ * @description:XX
+ **/
+public class MvcConfig {
+}
