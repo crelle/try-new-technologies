2ac1e0bb518a4e1d97b0051051c484efeb4e219f
e <383362161@qq.com>
2020-10-09 16:15:29 +0800

    spring session and security

--- spring-projects-reference-test/spring-security-refrence-test/src/main/java/crelle/test/spring/security/reference/demo/MvcConfig.java
+++ spring-projects-reference-test/spring-security-refrence-test/src/main/java/crelle/test/spring/security/reference/demo/MvcConfig.java
@@ -1,5 +1,9 @@
 package crelle.test.spring.security.reference.demo;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 /**
  * @author:crelle
  * @className:MvcConfig
@@ -7,5 +11,14 @@ package crelle.test.spring.security.reference.demo;
  * @date:2020/10/9
  * @description:XX
  **/
-public class MvcConfig {
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
 }
--- spring-projects-reference-test/spring-security-refrence-test/src/main/java/crelle/test/spring/security/reference/demo/WebSecurityConfig.java
+++ spring-projects-reference-test/spring-security-refrence-test/src/main/java/crelle/test/spring/security/reference/demo/WebSecurityConfig.java
@@ -1,5 +1,15 @@
 package crelle.test.spring.security.reference.demo;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
 /**
  * @author:crelle
  * @className:WebSecurityConfig
@@ -7,5 +17,34 @@ package crelle.test.spring.security.reference.demo;
  * @date:2020/10/9
  * @description:XX
  **/
-public class WebSecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "/home").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsService() {
+        UserDetails user =
+                User.withDefaultPasswordEncoder()
+                        .username("user")
+                        .password("password")
+                        .roles("USER")
+                        .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
 }
